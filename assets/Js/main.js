a574dd48de1541999de31a6f58543deb73c040d1
ianfr93 <albitox10@gmail.com>
2024-03-10 16:27:27 -0300

    cambios

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,27 +1,63 @@
 let usuarios = [{
-    usuario: "usuario1",
-    contraseña: "contraseña1"
-  },
-  {
-    usuario: "usuario2",
-    contraseña: "contraseña2"
-  },
-  {
-    usuario: "usuario3",
-    contraseña: "contraseña3"
-  },
-  {
-    usuario: "usuario4",
-    contraseña: "contraseña4"
-  },
-  {
-    usuario: "usuario5",
-    contraseña: "contraseña5"
-  },
-  {
-    usuario: "usuario6",
-    contraseña: "contraseña6"
+  usuario: "usuario1",
+  contraseña: "contraseña1",
+  perfil: {
+    nombre: "Ian Faúndez Rubio",
+    rut: "18408203-9",
+    correo: "Ifaundez.a@hotmail.com",
+    direccion: "Emiliano Zapata 693"
   }
+},
+{
+  usuario: "usuario2",
+  contraseña: "contraseña2",
+  perfil: {
+    nombre: "juan fernandez calvo",
+    rut: "13402203-9",
+    correo: "juanito.c@hotmail.com",
+    direccion: "Santiago,centro"
+  }
+},
+{
+  usuario: "usuario3",
+  contraseña: "contraseña3",
+  perfil: {
+    nombre: "marcelo rios",
+    rut: "10708223-9",
+    correo: "Chinorrios@hotmail.com",
+    direccion: "lo barnechea las casas 45"
+  }
+},
+{
+  usuario: "usuario4",
+  contraseña: "contraseña4",
+  perfil: {
+    nombre: "gabriel prieto",
+    rut: "11408203-9",
+    correo: "Ifaundez.a@hotmail.com",
+    direccion: "Nuñoa 456"
+  }
+},
+{
+  usuario: "usuario5",
+  contraseña: "contraseña5",
+  perfil: {
+    nombre: "luis miguel",
+    rut: "11402303-9",
+    correo: "luismi.a@hotmail.com",
+    direccion: "coquimbo 345"
+  }
+},
+{
+  usuario: "usuario6",
+  contraseña: "contraseña6",
+  perfil: {
+    nombre: "nicolas massu",
+    rut: "13408103-1",
+    correo: "nicolas.massu@hotmail.com",
+    direccion: "la florida 233"
+  }
+}
 ];
 
 let intentosRestantes = 5;
