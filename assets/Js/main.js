3045c5218afc2bc2d8944261aa51b6ce4da21664
ianfr93 <albitox10@gmail.com>
2024-03-12 18:21:51 -0300

    cambios

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -242,7 +242,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-
 document.addEventListener("DOMContentLoaded", function () {
   const contenedor = document.querySelector('.contenedor');
   const menuToggle = document.querySelector('.menu-toggle');
