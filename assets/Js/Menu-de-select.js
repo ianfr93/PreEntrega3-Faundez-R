3dd63769fed0aa518dfec363b1ea9cf359682770
ianfr93 <albitox10@gmail.com>
2024-03-17 17:39:42 -0300

    js

--- assets/Js/Menu-de-select.js
+++ assets/Js/Menu-de-select.js
@@ -1,38 +1,39 @@
+function obtenerValorSelect(idSelect) {
+  const selectElement = document.getElementById(idSelect);
+  return selectElement.options[selectElement.selectedIndex].text;
+}
+
+function validarCampos() {
+  empresa = obtenerValorSelect('selectEmpresa');
+  caja = obtenerValorSelect('selectCaja');
+  imprimir = obtenerValorSelect('selectImprimir');
+  tipoDocumento = obtenerValorSelect('selectDocumento');
+  monto = parseFloat(document.getElementById('monto').value);
+
+  switch (true) {
+      case (empresa === 'Seleccione' || caja === 'Seleccione' || imprimir === 'Seleccione' || tipoDocumento === 'Seleccione'):
+          alert('Por favor, seleccione todas las opciones antes de continuar.');
+          return false;
+      case (monto <= 0 || isNaN(monto)):
+          alert('Ingrese un monto válido mayor que cero.');
+          return false;
+      default:
+          return true;
+  }
+}
+
+// Definición de la función guardarYRedirigir
+function guardarYRedirigir() {
+  if (validarCampos()) {
+      alert('Datos válidos. Guardando y redirigiendo desde la pantalla de caja...');
+      window.location.href = './dashboard.html';
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  function obtenerValorSelect(idSelect) {
-      const selectElement = document.getElementById(idSelect);
-      return selectElement.options[selectElement.selectedIndex].text;
-  }
-
-  function validarCampos() {
-      empresa = obtenerValorSelect('selectEmpresa');
-      caja = obtenerValorSelect('selectCaja');
-      imprimir = obtenerValorSelect('selectImprimir');
-      tipoDocumento = obtenerValorSelect('selectDocumento');
-      monto = parseFloat(document.getElementById('monto').value);
-
-      switch (true) {
-          case (empresa === 'Seleccione' || caja === 'Seleccione' || imprimir === 'Seleccione' || tipoDocumento === 'Seleccione'):
-              alert('Por favor, seleccione todas las opciones antes de continuar.');
-              return false;
-          case (monto <= 0 || isNaN(monto)):
-              alert('Ingrese un monto válido mayor que cero.');
-              return false;
-          default:
-              return true;
-      }
-  }
-
-  // Definición de la función guardarYRedirigir
-  function guardarYRedirigir() {
-      if (validarCampos()) {
-          alert('Datos válidos. Guardando y redirigiendo desde la pantalla de caja...');
-          window.location.href = './dashboard.html';
-      }
-  }
-
   // Aquí puedes agregar cualquier otro código que necesites dentro del evento DOMContentLoaded
 });
+
 
 
 
